10340db1b753ea835dcce6a4c58738004270c77d
Vadym Haimanovskyi <v.haimanovskyi@inchkiev.ua>
2021-10-18 17:35:27 +0300

    push build

---
+++ precache-manifest.54d934a9a21d7675e92d90f7a052e3f4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fe912d87f68441893d",
+    "url": "/vue-schadule/css/app.3884747f.css"
+  },
+  {
+    "revision": "021b27adbe95b2608555",
+    "url": "/vue-schadule/css/chunk-ecc32c86.c92d5899.css"
+  },
+  {
+    "revision": "07aacf23bd7d55b35d8cabd68eb75d15",
+    "url": "/vue-schadule/fonts/DiariaSansPro-Regular.07aacf23.woff2"
+  },
+  {
+    "revision": "8d8b0f01c10dc7c4a98ef58ebb2851f3",
+    "url": "/vue-schadule/fonts/DiariaSansPro-Regular.8d8b0f01.woff"
+  },
+  {
+    "revision": "d1447f171cac619628aea4124571921e",
+    "url": "/vue-schadule/index.html"
+  },
+  {
+    "revision": "f5fe912d87f68441893d",
+    "url": "/vue-schadule/js/app.cfd7524f.js"
+  },
+  {
+    "revision": "021b27adbe95b2608555",
+    "url": "/vue-schadule/js/chunk-ecc32c86.1bc10cb6.js"
+  },
+  {
+    "revision": "ac49467a1db59be45c5c",
+    "url": "/vue-schadule/js/chunk-vendors.0fffec53.js"
+  },
+  {
+    "revision": "c2bcbfe999a953a5a56a9e55a86f5905",
+    "url": "/vue-schadule/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-schadule/robots.txt"
+  }
+]);
